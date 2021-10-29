b78fa03a9a3bb9121684c85c07973418507e60a6
yattdev <yattdeveloper@gmail.com>
2021-10-29 16:11:41 +0100

    Included api urls to backend/urls

--- backend/urls.py
+++ backend/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # third party apps urls
+    path('api/v1/', include('api.urls')),
 ]
+
+# enables django to know location of static and media files
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
