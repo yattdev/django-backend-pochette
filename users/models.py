1cb654478e8bd22be4ec7702428fb0ccd10de7ef
yattdev <yattdeveloper@gmail.com>
2021-10-29 15:53:04 +0100

    Override default UserManager and AbstratUser to enable email as username

--- users/models.py
+++ users/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
 # Create your models here.
+
+
+class UserAccountManager(UserManager):
+    """ DOCSTRING:
+        Define a custom user manager
+    """
+
+    # override create user method to accept email as username
+    def create_user(self, email=None, password=None, **extra_fields):
+        return super().create_user(email,
+                                   email=email,
+                                   password=password,
+                                   **extra_fields)
+
+    # override createusuperuser method to accept email as username
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        return super().create_superuser(email,
+                                        email=email,
+                                        password=password,
+                                        **extra_fields)
+
+
+
+class UserAccount(AbstractUser):
+    """ DOCSTRING:
+        define a custom user models
+    """
+    email = models.EmailField(unique=True)
+    
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = UserAccountManager()
+
+    class Meta:
+        ordering = ['-date_joined']
+        
