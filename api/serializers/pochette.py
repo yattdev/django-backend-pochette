085142808d84fc8523649ff7a595c69fce056a1e
yattdev <yattdeveloper@gmail.com>
2021-11-02 01:16:07 +0100

    Create serializers for Pochette model from gestion_pochette app

---
+++ api/serializers/pochette.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from rest_framework import serializers
+from gestion_prochette.models import Pochette
+
+
+class PochetteSerializer(serializers.ModelSerializer):
+    """ Serialize Pochette models from gestion_prochette """
+    author = serializers.StringRelatedField()
+    author_id = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Pochette
+        fields = (
+            'id',
+            'author',
+            'title',
+            'author_id',
+            'image',
+            'is_public',
+            'slug',
+        )
