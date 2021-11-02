f1c94a496b73f0753a3572616abe20369fadbb82
yattdev <yattdeveloper@gmail.com>
2021-11-02 14:19:55 +0100

    Switch database from db.sqlite3 to postgresql with pgloader

--- backend/settings.py
+++ backend/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'album_pochette',
+        'USER': 'alassane',
+        'PASSWORD': '7678_gone',
+        'HOST': '',
+        'PORT': 5432
     }
 }
 
