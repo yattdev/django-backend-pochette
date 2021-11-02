42540bd525e7ccc84d6968942988ee6f65ca5def
yattdev <yattdeveloper@gmail.com>
2021-11-02 01:57:46 +0100

    Added endpoint get-token to retrieve a refresh csrf token when post a form

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from django.urls import path, include
+from .views import PochetteList, PochetteDetails, get_csrf_token
 
 urlpatterns = [
     path('pochettes/', PochetteList.as_view(), name='pochette-list'),
     path('pochettes/<int:pk>/<slug:slug>', PochetteDetails.as_view(), name='pochette-details'),
+    path('get-token/', get_csrf_token, name="get-token"), # to get csrf_token 
     # third party apps urls
     path('auth', include('djoser.urls'), name="auth"),
     path('auth', include('djoser.urls.jwt')),
